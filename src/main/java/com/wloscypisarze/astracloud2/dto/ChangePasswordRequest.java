66cff1980213fe344c53f283d7e2bb40243e09af
Robert Tomala <robert0604@o2.pl>
2026-06-15 23:48:17 +0200

    regex dla hasła zgodny z deklaracją projektową

--- src/main/java/com/wloscypisarze/astracloud2/dto/ChangePasswordRequest.java
+++ src/main/java/com/wloscypisarze/astracloud2/dto/ChangePasswordRequest.java
@@ -1,6 +1,7 @@
 package com.wloscypisarze.astracloud2.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,6 +17,10 @@ public class ChangePasswordRequest {
     @Setter
     @NotBlank(message = "Hasło jest wymagane")
     @Size(min = 8, message = "Nowe hasło musi mieć co najmniej 8 znaków")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
+            message = "Hasło musi zawierać co najmniej 8 znaków, w tym cyfrę i znak specjalny"
+    )
     private String newPassword;
 
 }
--- src/main/java/com/wloscypisarze/astracloud2/dto/RegisterRequest.java
+++ src/main/java/com/wloscypisarze/astracloud2/dto/RegisterRequest.java
@@ -3,6 +3,7 @@ package com.wloscypisarze.astracloud2.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,6 +26,10 @@ public class RegisterRequest {
     @Getter
     @NotBlank(message = "Hasło jest wymagane")
     @Size(min = 8, message = "Hasło musi mieć co najmniej 8 znaków")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
+            message = "Hasło musi zawierać co najmniej 8 znaków, w tym cyfrę i znak specjalny"
+    )
     private String password;
 
     @Setter
